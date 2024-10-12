fd378f2f01ba941d500418d09f3b41a2b5bcb85d
setifmohamedsaoudi <setifmohamedsaoudi@gmail.com>
2024-10-12 16:44:32 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,6 @@
 <?php
-// logout.php
 session_start();
-session_unset();
-session_destroy();
-header("Location: login.php");
+session_destroy(); // تدمير الجلسة
+header("Location: index.php"); // إعادة توجيه المستخدم إلى الصفحة الرئيسية
 exit();
 ?>
